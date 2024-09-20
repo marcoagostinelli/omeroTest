4a36b6120efc0c1e098c3427cdfe24846602aaa9
marcoagostinelli <70527916+marcoagostinelli@users.noreply.github.com>
2024-09-20 12:01:12 -0400

    added regexTest and HTD_practice

---
+++ HTD_practice.py
@@ -0,0 +1,119 @@
+import os
+import ezomero.json_api
+import omero.clients
+import ezomero
+import omero.gateway
+import omero.model
+import omero.scripts as scripts
+import omero
+import json
+from omero.gateway import BlitzGateway
+from omero.gateway import DatasetWrapper
+from omero.gateway import ScreenWrapper
+from omero.gateway import PlateWrapper
+from omero.model import ScreenI
+from omero.model import PlateI
+from omero.model import WellI
+from omero.model import ImageI
+from omero.model import WellSampleI
+from omero.model import ScreenPlateLinkI
+from omero.rtypes import rint, rlong, rstring, robject, unwrap
+
+# This program will collect info from a HTD file
+
+fileLocation = "Plate35Plcg2.HTD"
+
+def parseContents(file):
+    data = {}
+    for line in file:
+        line = line.strip()
+        if line == '"EndFile"':
+            break
+        key, *value = line.split(', ')
+        key = key.strip('"')
+
+        # Handling boolean values
+        value = [v.strip() == 'TRUE' for v in value] if any(v in ('TRUE', 'FALSE') for v in value) else [v.strip('"') for v in value]
+
+        # Flatten lists if there's only one element
+        if len(value) == 1:
+            value = value[0]
+        data[key] = value
+    return data
+
+# Read file contents of HTD and convert to JSON
+def HTD_to_JSON(fileLocation):
+    file = open(fileLocation, "r")
+    parsedData = parseContents(file)
+
+    # Convert the dictionary to JSON format
+    jsonData = json.dumps(parsedData,indent=4)
+    return json.loads(jsonData)
+
+#returns a list of wavelength names
+def getWaveLengthData(jsonFile):
+    size = jsonFile.get("NWavelengths")
+
+    # store the wavelength names in a list
+    wavelengthNames = []
+
+    # get the names of each wavelength
+    for i in range(int(size)):
+        wavelengthNames.append(jsonFile.get("WaveName"+str(i+1)))
+    return wavelengthNames
+
+#returns a list of the well names in use ex: A01, A03...
+def getWells(jsonFile):
+    wells = []
+
+    #the column is the number
+    xWells = jsonFile.get("XWells")
+
+    #the row is the letter
+    yWells = jsonFile.get("YWells")
+    
+    for i in range(int(yWells)):
+        for j in range(int(xWells)):
+            #check each well if it is used or not
+            check = jsonFile.get("WellsSelection"+str(i+1))[j]
+            if check is True:
+                #get the corresponding letter for our number
+                letter = chr(i + ord('A'))
+
+                num = ""
+                #make sure every number used is at least 2 digits. A1 -> A01
+                if j < 10:
+                    num = str(0) + str(j)
+                else:
+                    num = str(j)
+                wells.append(letter+num)
+    return wells
+
+# Constructs a json file based on a given HTD file
+def constructHTDInfo(fileLocation):
+    #writes HTD data to a json object for processing
+    data = HTD_to_JSON(fileLocation)
+
+    wellsList = getWells(data)
+    waveList = getWaveLengthData(data)
+
+    #create JSON object for output
+    info = {}
+    info['wavelength'] = {"number":len(waveList), "names":waveList}
+    info['wells'] = wellsList
+    #check if there are multiple sites
+    if data.get("XSites"):
+        info['sites'] = int(data.get("XSites")) * int(data.get("YSites"))
+    else:
+        info['sites'] = 1
+    
+    jsonFile = open("testing.json", 'w')
+    json.dump(info, jsonFile, indent=4)
+    return info
+
+info = constructHTDInfo(fileLocation)
+print("D02" in info['wells'])
+
+
+
+
---
+++ regexTest.py
@@ -0,0 +1,151 @@
+import os
+import HTD_practice
+import ezomero.json_api
+import omero.clients
+import ezomero
+import omero.gateway
+import omero.model
+import omero.scripts as scripts
+import omero
+import re
+import json
+from omero.gateway import BlitzGateway
+from omero.gateway import DatasetWrapper
+from omero.gateway import ScreenWrapper
+from omero.gateway import PlateWrapper
+from omero.model import ScreenI
+from omero.model import PlateI
+from omero.model import WellI
+from omero.model import ImageI
+from omero.model import WellSampleI
+from omero.model import ScreenPlateLinkI
+from omero.rtypes import rint, rlong, rstring, robject, unwrap
+
+directory = 'IF'
+
+# set the HTD file that is being used in this project
+htd = HTD_practice.constructHTDInfo("Plate35Plcg2.HTD")
+
+pattern = '(.+)_([A-Z]\d+)_(s\d+)_(w\d+)'
+word = "plate333w_Q22_s22_w43"
+match = re.match(pattern,word)
+
+#This function returns a json that holds all possible wells/wavelengths/sites. Used for determining incomplet or missing wells
+def getAllWells(htd):
+    allWells = {}
+
+    # number of sites in htd file
+    siteCount = list(range(1,htd['sites']+1))
+    # number of waves in htd file
+    waveCount = htd['wavelength']['number']
+    
+
+    #populate the complete list of wells
+    for well in htd['wells']:
+        allWells[well] = {}
+        for i in range(waveCount):
+            allWells[well]["w"+str(i+1)] = {
+                "Name": htd['wavelength']['names'][i],
+                "Sites" : list(siteCount)
+            }
+    return allWells
+
+#This is used to remove all used wells from the list of total wells, 
+#leaving us with only the incomplete wells
+def subtractJson(all,used):
+    for well in used:
+        #if the well is used, check if all wavelengths are there
+        for wavelength in used[well]:
+            
+            #if the wavelength is used, check if all sites are there
+            for site in used[well][wavelength]['Sites']:
+                if (site in used[well][wavelength]['Sites']):
+                    all[well][wavelength]['Sites'].remove(site)
+                
+                #if the wavelength is now empty, remove it
+                if not all[well][wavelength]['Sites']:
+                    del all[well][wavelength]
+                
+                #now, if the well is empty, remove it
+                if not all[well]:
+                    del all[well]
+    return all
+
+
+
+
+
+#matches every image in a folder to a regex and adds each part to the valid or refused JSON object
+def getImages(directory,regex):
+    validImages = {}
+    refusedImages = {}
+
+    for filename in os.listdir(directory):
+        # verify the name matches the regex
+        match = re.match(regex,filename)
+        #(plateName_well_site_wavelength)
+        if match:
+            plateName, well, site, wavelength = match.groups()
+            
+            # add the well if it doesn't exist in the valid json object
+            if well not in validImages and well in htd['wells']:
+                validImages[well] = {}
+            #add to refused images
+            elif well not in refusedImages and well not in htd['wells']:
+                refusedImages[well] = {}
+
+
+            #add the wavelength to the appropriate object if it has not been added yet
+            if (well in validImages):
+                # add the wavelength if it does not exist and is valid
+                if (wavelength not in validImages[well]) and (int(wavelength[1:]) <= htd['wavelength']['number']):
+                    validImages[well][wavelength] = {
+                        "Name" : htd['wavelength']['names'][int(wavelength[1:]) -1],
+                        "Sites" : {}
+                    }
+            else:
+                if (wavelength not in refusedImages[well]):
+                    refusedImages[well][wavelength] = {
+                        #There is no name that exists for refused wells
+                        "Sites": {}
+                    }
+            #finally, add the site to the appropriate object if it has not been added yet
+            if (int(site[1:]) <= htd['sites']) and (well in validImages):
+                    if (int(site[1:]) not in validImages[well][wavelength]["Sites"]):
+                        validImages[well][wavelength]["Sites"][int(site[1:])] = {
+                            "filename": filename,
+                            #TODO find out new filename
+                            "newFilename": filename,
+                            "filePath" : directory,
+                            "wellId" : well,
+                            "siteId" : site,
+                            "waveLengthId" : wavelength
+                        }
+
+            else:
+                if (int(site[1:]) not in refusedImages[well][wavelength]["Sites"]):
+                    refusedImages[well][wavelength]["Sites"][int(site[1:])] = {
+                        "filename": filename,
+                        #TODO find out new filename
+                        "newFilename" : filename,
+                        "filePath" : directory,
+                        "wellId" : well,
+                        "siteId" : site,
+                        "waveLengthId" : wavelength
+                    }          
+    return validImages, refusedImages
+
+            
+
+validWells,invalidWells = getImages(directory,pattern)
+allWells = getAllWells(htd)
+
+incompleteWellsJson = open("incomplete.json", "w")
+validJson = open("valid.json", 'w')
+refusedJson = open("refused.json","w")
+
+json.dump(validWells, validJson, indent=4)
+json.dump(invalidWells, refusedJson, indent=4)
+#subtract valid wells from all wells to get incomplete wells
+incompleteWells = subtractJson(allWells,validWells)
+json.dump(incompleteWells,incompleteWellsJson, indent=4)
