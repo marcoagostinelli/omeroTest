c1f189735ba4211a03a586927d2ff7d7d74f18f0
marcoagostinelli <marcoagostinelli2@hotmail.com>
2024-10-22 11:35:07 -0400

    populate the new excel file

--- excelPractice.py
+++ excelPractice.py
@@ -6,6 +6,8 @@ from HTD_practice import getHtdFile
 from regexTest import getImages
 from regexTest import getIncompleteWells
 from regexTest import getValidImageNames
+from regexTest import checkName
+import json
 #TODO uncomment this line when omero is installed on python
 #from regexTest import checkName
 import re
@@ -45,6 +47,7 @@ def walk_files(root, extensions):
     """
     for dirpath, dirnames, files in os.walk(root, topdown=True):
         for file in files:
+            timepoint,zStep = getTandZ(dirpath)
             filename, extension = os.path.splitext(file)
             if extension in extensions:
                 filepath = os.path.join(dirpath, file)
@@ -54,11 +57,12 @@ def walk_files(root, extensions):
                     name, ext = os.path.splitext(f)
                     if (ext == ".json") and (name.lower().count(file.lower()) > 0):
                         json = f
-                yield (file, filepath, json)
+                
+                yield (file, filepath, json, timepoint, zStep)
     
 
 
-def create_DataFrame(root,valid, extensions, columns=["File Name","New File Name","File Path","MMA File Path","Plate Name", "Well ID", "Site ID", "Wavelength ID"]):
+def create_DataFrame(root,valid, extensions,htd, columns=["Well_Name","IMAGE NAME","File Name","File Path","MMA FILE PATH","Site ID", "Wavelength ID", "Z Score", "TimePoint"]):
     """Create a DataFrame containing image file info for the root directory.
 
     DataFrame will contain file name, new file name constructed by replacing backslashes in filepath
@@ -68,6 +72,7 @@ def create_DataFrame(root,valid, extensions, columns=["File Name","New File Name
         root: path to a dataset directory continaing subdirectories and image files.
         valid: a list of valid image names. Only images in this list will be added to the excel file
         extensions: list of string image file extensions.
+        htd: the htd file dictionary used
         columns: column names used to create the DataFrame and CSV output.
      Returns:
         A DataFrame containing information written to the output csv file.
@@ -76,17 +81,27 @@ def create_DataFrame(root,valid, extensions, columns=["File Name","New File Name
     """
     df = pd.DataFrame(columns=columns)
     walk = walk_files(root, extensions)
-    for file, filepath, json in walk:
+    for file, filepath, json, timepoint, zStep in walk:
 
         #check if the image is valid before adding it to the excel file
         if file in valid:
             #TODO use this function when testing on a machine with omero installed in python
             #match = checkName(file)   
 
-            #TODO remove this line when omero is installed on python
-            match = re.match("(.+)_([A-Z]\d+)_(s\d+)_(w\d+)",file)
-
-            platename, wellId, siteId, wavelengthId = match.groups()
+            match = checkName(file, htd)
+            # if there is 1 wavelength and/or site, then set it to 1 because it will not be included in the filename
+            if htd["sites"] == 1 and htd["wavelength"]["number"] == 1:
+                plateName, well, = match.groups()
+                site = "s1"
+                wavelength = "w1"
+            elif htd["sites"] == 1:
+                plateName, well, wavelength = match.groups()
+                site = "s1"
+            elif htd["wavelength"]["number"] == 1:
+                plateName, well, site = match.groups()
+                wavelength = "w1"
+            else:
+                plateName, well, site, wavelength = match.groups()
 
             # seperate the directories in the path from the ending file
             dirpath = os.path.dirname(filepath)
@@ -113,7 +128,8 @@ def create_DataFrame(root,valid, extensions, columns=["File Name","New File Name
                 new_filename = truncate_name(dirs)
 
             # add row to the DataFrame
-            row = pd.DataFrame([[file, new_filename, filepath, json, platename, wellId, siteId, wavelengthId]], columns=columns)
+            #TODO: find out image name and MMA File path
+            row = pd.DataFrame([[well,new_filename,file,filepath,json,site,wavelength,zStep,timepoint]],columns=columns)
             df = df._append(row, ignore_index=True)
         
     df.sort_values(by="File Name", inplace=True)
@@ -121,7 +137,7 @@ def create_DataFrame(root,valid, extensions, columns=["File Name","New File Name
 
 
 # NOTE: : doesn't close file after writing
-def write_excel(file, df, sheet_number=2, cell="A14"):
+def write_excel(file, df, sheet_number=3, cell="A2"):
     """Write DataFrame to an existing excel file on a specific sheet starting at a specific cell.
 
      Args:
@@ -135,10 +151,20 @@ def write_excel(file, df, sheet_number=2, cell="A14"):
     """
     wb = xw.Book(file)
     sheet = wb.sheets[sheet_number]
+
+    # Clear cells starting from the specified cell to the end of the used range
+    clearStart = sheet.range(cell).address
+    usedRange = sheet.used_range
+
+    # define the range to clear
+    clearRange = sheet.range(clearStart,(usedRange.last_cell.row,usedRange.last_cell.column))
+    clearRange.clear()
+
+
     sheet[cell].options(index=False, header=False).value = df
     wb.save(file)
 
-def read_excel(file, dataset_sheet=1, image_list_sheet=2, dataset_cell="C10", image_list_cell="B10"):
+def read_excel(file, dataset_sheet=2, image_list_sheet=2, dataset_cell="B9", image_list_cell="B10"):
     """Read an excel file and extract dataset folder name and image file extensions from specific cells.
 
      Args:
@@ -161,20 +187,39 @@ def read_excel(file, dataset_sheet=1, image_list_sheet=2, dataset_cell="C10", im
     extensions = extensions.split(" ")
     return (dataset, extensions)
 
+#with a given directory eg. dataset5\Timepoint_1\ZStep_1
+#extract the timepoint and ZStep. Covers all possible folder structure outcomes (missing ZStep folder or Timepoint folder...)
+def getTandZ(dirpath):
+    timepoint = ""
+    zStep = ""
+    parts = dirpath.split("\\")
+    if len(parts) == 1:
+        timepoint = "TimePoint_1"
+        zStep = "ZStep_1"
+    elif len(parts) == 3:
+        timepoint= parts[1]
+        zStep = parts[2]
+    elif len(parts) == 2:
+        if parts[1].split("_")[0] == "ZStep":
+            zStep = parts[1]
+            timepoint = "TimePoint_1"
+        elif parts[1].split("_")[0] == "TimePoint":
+            timepoint = parts[1]
+            zStep = "ZStep_1"
+    #return just the number of the timepoint and zStep
+    return timepoint.split("_")[1], zStep.split("_")[1]
 
 # MAIN
 def main():
+    excel = "POST-PUB_YCHAROS_template_noMacros_v16_Oct08_JL_CS_SPW_.xlsm"
 
-    excel = "Pazour_OMERO_import_template_wMacros_v06.xlsm"
-    directory = "IF"
     dataset, extensions = read_excel(excel)
-    print(extensions[1])
 
     #get htd file
-    htd = getHtdFile(directory)
+    htd = getHtdFile(dataset)
 
     #using the htd file, get dictionary of valid and refused images. Retreive incomplete wells too
-    validImages,rejectedImages = getImages(directory,htd)
+    validImages,rejectedImages = getImages(dataset,htd)
     incomplete = getIncompleteWells(htd, validImages)
 
     #TODO add error if there is something in rejectedImages or incomplete.
@@ -184,7 +229,7 @@ def main():
     #get list of image names to be displayed on excel file
     validImageNames = getValidImageNames(validImages)
 
-    df = create_DataFrame(directory,validImageNames,extensions)
+    df = create_DataFrame(dataset,validImageNames,extensions,htd)
     write_excel(excel,df)
 
 if __name__ == "__main__":
